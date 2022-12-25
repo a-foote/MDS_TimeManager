d1cc415cb7ac294cc6a0c3982cb450b162b6afd3
a-foote <alyssa.k.foote@gmail.com>
2022-12-24 22:34:19 -0800

    fixing imports in test files

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,6 @@ import os
 import sys
 PROJECT_PATH = os.getcwd()
 SOURCE_PATH = os.path.join(
-    PROJECT_PATH,"MDSTimeManager"
+    PROJECT_PATH,"../MDSTimeManager/"
 )
 sys.path.append(SOURCE_PATH)
