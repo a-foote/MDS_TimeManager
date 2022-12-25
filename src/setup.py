7a54eec540350ae165e1570afa551aff60ef0256
rishi71095 <vjsngh08@gmail.com>
2022-12-24 20:22:36 -0800

    adding src folder (not sure if required)

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MDSTimeManager',
+    version='1.2',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Project created by Vimaljeet Singh & Alyssa for Object Oriented Programming course.',
+	url="https://github.com/a-foote/MDS_TimeManager",
+    author="Vimaljeet Singh & Alyssa Foote",
+    author_email='vjsngh08@gmail.com'
+)
